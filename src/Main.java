17335380d3c5452aa254d2b0ddbbc911f2d1e4b1
Aleksandr Kretov <kretov_a@outlook.com>
2022-02-04 10:32:36 +0300

    reformat

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,20 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         int creditSumm = 1_000_000;
         double percent = 9.9;
-        int numberOfMonths = 36;
+        int numberOfMonths = 12;
         double cps = service.calculate(creditSumm, percent, numberOfMonths);
-        System.out.println("Ежемесячный платеж составит - " + Math.round(cps + 41.5) + "рубля(ей)");
+        System.out.println("При сумме кредита 1000000 рублей, под 9,9 % годовых, сроком на " + numberOfMonths +
+                " месяцев ,ежемесячный платеж составит - " + Math.round(cps + 41.5) + "рубля(ей)");
+        int numberOfMonths2 = 24;
+        double cps2 = service.calculate(creditSumm, percent, numberOfMonths2);
+        System.out.println("При сумме кредита 1000000 рублей, под 9,9 % годовых, сроком на " + numberOfMonths2 +
+                " месяцев ,ежемесячный платеж составит - " + Math.round(cps2 + 41.5) + "рубля(ей)");
+        int numberOfMonths3 = 36;
+        double cps3 = service.calculate(creditSumm, percent, numberOfMonths3);
+        System.out.println("При сумме кредита 1000000 рублей, под 9,9 % годовых, сроком на " + numberOfMonths3 +
+                " месяцев ,ежемесячный платеж составит - " + Math.round(cps3 + 41.5) + "рубля(ей)");
 
-        // 41.5 рубль оседает на чьем то счету. И так с каждого платежа, каждого клиента! Дело раскрыто =)git
+        // 41.5 рубль оседает на чьем то счету. И так с каждого платежа, каждого клиента! Дело раскрыто =)
     }
 }
 
